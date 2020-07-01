e86f669785f21880507c8d62c669389b3fa9f0ac
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-01 22:22:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ logWhisper('JAVASCRIPT');
 
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) {
+  if (string === string.toLowerCase()) {
     return "I can't hear you!"
-  } else if (string.toUpperCase) {
+  } else if (string === string.toUpperCase) {
     return "YES INDEED!"
   } else if (string = "I love you, Grandma.") {
     return "I love you, too."
